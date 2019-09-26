78a422598faae8bfc0bdcc53f7235c0fd85211eb
pushkar-saraf <pushkar.saraf.ossu@gmail.com>
2019-09-26 14:49:53 +0530

    final

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -1,12 +1,6 @@
 package com.stackroute;
 
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.StaticApplicationContext;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 
 /**
  * Hello world!
@@ -17,32 +11,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        useBeanFactory();
         useAppContext();
-        useBeanDefinition();
     }
 
     private static void useAppContext() {
         System.out.println("Using ApplicationContext");
         ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
                 "beans.xml");
-        Movie movie = (Movie) appContext.getBean("movie");
-        System.out.println(movie.getActor());
-    }
-
-    private static void useBeanFactory() {
-        System.out.println("Using BeanFactory");
-        Resource res = new FileSystemResource("src/main/resources/beans.xml");
-        XmlBeanFactory factory = new XmlBeanFactory(res);
-        Movie movie = (Movie) factory.getBean("movie");
-        System.out.println(movie.getActor());
-    }
-
-    private static void useBeanDefinition() {
-        System.out.println("Using BeanDefinition");
-        StaticApplicationContext staticApplicationContext = new StaticApplicationContext();
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(staticApplicationContext);
-        Resource res = new FileSystemResource("src/main/resources/beans.xml");
-        reader.loadBeanDefinitions(res);
+        Movie beanA = (Movie) appContext.getBean("MovieA");
+        Movie beanB = (Movie) appContext.getBean("MovieB");
+        System.out.println(beanA == beanB);
     }
 }
