5415b7a0c6fc57d25f6a65c6517e916d28552fa4
pushkar-saraf <pushkar.saraf.ossu@gmail.com>
2019-09-26 14:31:42 +0530

    new

--- src/main/java/com/stackroute/Movie.java
+++ src/main/java/com/stackroute/Movie.java
@@ -3,6 +3,10 @@ package com.stackroute;
 public class Movie {
     private Actor actor;
 
+    public Movie(Actor actor){
+        this.actor = actor;
+    }
+
     public void setActor(Actor actor) {
         this.actor = actor;
     }
