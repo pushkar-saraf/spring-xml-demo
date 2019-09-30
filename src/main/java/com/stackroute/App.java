0583fb45b8e0c4bccb97711aed34d00d4fbf380f
pushkar-saraf <pushkar.saraf.ossu@gmail.com>
2019-09-30 15:06:33 +0530

    resolved issues

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -2,12 +2,8 @@ package com.stackroute;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         useAppContext();
     }
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.domain;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
