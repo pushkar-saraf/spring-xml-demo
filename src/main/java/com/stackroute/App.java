6461bec8e15f49e90a5c75eef8fcbde201c415b1
pushkar-saraf <pushkar.saraf.ossu@gmail.com>
2019-09-30 14:42:32 +0530

    resolved issues

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -2,9 +2,6 @@ package com.stackroute;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         useAppContext();
