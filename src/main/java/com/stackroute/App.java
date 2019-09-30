fdd27a225d7971d9d1c9f1bc2187a4f501a27258
pushkar-saraf <pushkar.saraf.ossu@gmail.com>
2019-09-30 14:55:31 +0530

    issues resolved

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -1,5 +1,6 @@
 package com.stackroute;
 
+import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
